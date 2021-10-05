f2bdd267b66f21497480799682f33fbbf0eb0bdc
Marco <gsoccerstar007@gmail.com>
2021-10-05 13:11:41 -0400

    buttons labeled and log info

---
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.basicphrases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void playPhrase(View view) {
+
+        Button buttonPressed = (Button) view;
+
+        Log.i("Button pressed", buttonPressed.getTag().toString());
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
